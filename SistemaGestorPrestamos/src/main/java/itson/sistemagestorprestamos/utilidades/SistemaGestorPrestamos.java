1b60e91058b7d1755b4667485a1d8aeb4dabc66c
Adel Mendez <adelmendez2@gmail.com>
2025-06-12 12:17:42 -0700

    Impresion en el main
    prueba de commit

--- SistemaGestorPrestamos/src/main/java/itson/sistemagestorprestamos/utilidades/SistemaGestorPrestamos.java
+++ SistemaGestorPrestamos/src/main/java/itson/sistemagestorprestamos/utilidades/SistemaGestorPrestamos.java
@@ -12,5 +12,6 @@ public class SistemaGestorPrestamos {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("hola plebillos");
     }
 }
