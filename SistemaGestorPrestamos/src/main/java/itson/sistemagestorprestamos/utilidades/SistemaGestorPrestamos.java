f2f34892615adb536d46581707d663d5af51d5ef
saulapodaca <152424794+saulapodaca@users.noreply.github.com>
2025-06-12 11:28:46 -0700

    Add files via upload

---
+++ SistemaGestorPrestamos/src/main/java/itson/sistemagestorprestamos/utilidades/SistemaGestorPrestamos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.sistemagestorprestamos.utilidades;
+
+/**
+ *
+ * @author saula
+ */
+public class SistemaGestorPrestamos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
